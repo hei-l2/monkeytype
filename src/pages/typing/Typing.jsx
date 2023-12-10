0d296413b669c79c2ab5e67324be3c8040b6d8ab
RickaPrincy <rckprincy@gmail.com>
2023-12-10 23:26:55 +0300

    feat: create text-show-ui

--- src/pages/typing/Typing.jsx
+++ src/pages/typing/Typing.jsx
@@ -3,9 +3,10 @@ import { Keyboard } from "./keyboard";
 import { TextShow } from "./text-show";
 
 export function Typing() {
+    const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates laborum sint delectus at neque. Nisi "
     return (
         <div className="d-c-c w-screen h-screen">
-            <TextShow />
+            <TextShow text={text} />
             <Keyboard layout={azerty} />
         </div>
     )
