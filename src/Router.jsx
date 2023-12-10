61419a1cc1d5b5cbd1903606898871efd6fa0a7e
RickaPrincy <rckprincy@gmail.com>
2023-12-10 21:22:27 +0300

    feat: create home page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from './pages';
+import { Home, Typing } from './pages';
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/typing" element={<Typing />} />
         </Routes>
     );
 }
---
+++ src/pages/typing/Typing.jsx
@@ -0,0 +1,5 @@
+export function Typing() {
+    return (
+        <div>Typing</div>
+    )
+}
