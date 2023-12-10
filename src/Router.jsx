f1319478aaec9e5fbc053d4e47b1a0a9b27d055b
RickaPrincy <rckprincy@gmail.com>
2023-12-10 21:31:28 +0300

    feat: create not found page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home, Typing } from './pages';
+import { Home, NotFound, Typing } from './pages';
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/typing" element={<Typing />} />
+            <Route path='/*' element={<NotFound />} />
         </Routes>
     );
 }
